ce222d2af3b20b45c447178e709acb92ee42ad9b
yangf6 <yangf6@uw.edu>
2017-02-22 10:57:44 -0800

    d

--- app/src/main/java/awty/yangf6/washington/edu/awty/MainActivity.java
+++ app/src/main/java/awty/yangf6/washington/edu/awty/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,16 +16,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     private boolean started;
 
     PendingIntent alarmIntent = null;
     BroadcastReceiver alarmReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(final Context context, Intent intent) {
-            String message = intent.getStringExtra("message");
-            String number = intent.getStringExtra("number");
-            Toast.makeText(MainActivity.this, number + ": " + message, Toast.LENGTH_SHORT).show();
+            String phn = intent.getStringExtra("phn");
+            String msg = intent.getStringExtra("msg");
+            Toast.makeText(MainActivity.this, phn + ": " + msg, Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -40,7 +39,6 @@ public class MainActivity extends ActionBarActivity {
         final EditText phoneNumber = (EditText) findViewById(R.id.phn);
         final EditText interval = (EditText) findViewById(R.id.freq);
 
-        final Activity currentActivity = this;
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,11 +63,10 @@ public class MainActivity extends ActionBarActivity {
                     int milliSeconds = Integer.parseInt(interval.getText().toString()) * 1000 * 60;
 
 
-
                     Intent i = new Intent();
-                    i.putExtra("number", phoneNumber.getText().toString());
-                    i.putExtra("message", message.getText().toString());
-                    i.setAction("SoundDaAlarm");
+                    i.putExtra("phn", phoneNumber.getText().toString());
+                    i.putExtra("msg", message.getText().toString());
+//                    i.setAction("SoundDaAlarm");
 
                     alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, i, 0);
                     am.setRepeating(AlarmManager.RTC, System.currentTimeMillis() + milliSeconds, milliSeconds, alarmIntent);
@@ -78,22 +75,22 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
